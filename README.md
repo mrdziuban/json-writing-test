b54f6411cc885d5d0aeabc5d132d2238e826d1dd
Matt Dziuban <mrdziuban@gmail.com>
2018-05-19 13:03:50 -0400

    Add JSON array of strings for comparison.

--- README.md
+++ README.md
@@ -8,25 +8,34 @@ incredibly slowly when the JSON contains an encoded JSON string. Here's the outp
 
 ```
 [info] Running example.JsonWriting
-Generating UUIDs took 275ms
-Generating argonaut JSON took 119ms
-Generating circe JSON took 110ms
+Generating UUIDs took 274ms
+Generating argonaut JSON took 131ms
+Generating circe JSON took 127ms
 
 **********************************************************************
 Argonaut
 **********************************************************************
 Writing plain text argonaut JSON with nospaces took 6ms
-Writing plain text argonaut JSON with spaces2 took 5ms
-Writing JSON text argonaut JSON with nospaces took 14452ms
-Writing JSON text argonaut JSON with spaces2 took 14278ms
+Writing plain text argonaut JSON with spaces2 took 6ms
+
+Writing JSON array argonaut JSON with nospaces took 17ms
+Writing JSON array argonaut JSON with spaces2 took 14ms
+
+Writing JSON text argonaut JSON with nospaces took 14316ms
+Writing JSON text argonaut JSON with spaces2 took 14122ms
+
 
 **********************************************************************
 Circe
 **********************************************************************
-Writing plain text circe JSON with nospaces took 4ms
+Writing plain text circe JSON with nospaces took 3ms
 Writing plain text circe JSON with spaces2 took 3ms
-Writing JSON text circe JSON with nospaces took 32ms
-Writing JSON text circe JSON with spaces2 took 19ms
+
+Writing JSON array circe JSON with nospaces took 11ms
+Writing JSON array circe JSON with spaces2 took 9ms
+
+Writing JSON text circe JSON with nospaces took 33ms
+Writing JSON text circe JSON with spaces2 took 15ms
 ```
 
 ## Running the example
--- src/main/scala/example/JsonWriting.scala
+++ src/main/scala/example/JsonWriting.scala
@@ -16,18 +16,26 @@ object JsonWriting {
   def testJsonWriting[J](tpe: String, json: J, writeNoSpaces: J => String, writeSpaces2: J => String): Unit = {
     timed(s"Writing $tpe JSON with nospaces", () => writeNoSpaces(json))
     timed(s"Writing $tpe JSON with spaces2", () => writeSpaces2(json))
+    println("")
   }
 
+  case class GeneratedJson[J](str: J, arr: J, encoded: J)
+
   /**
-   * Returns a tuple of two JSON objects using the given UUIDS.
+   * Returns a `GeneratedJson` object using the given UUIDS.
    *
-   * The first element is a JSON object containing one key/value pair of
+   * The `str` element is a JSON object containing one key/value pair of
    * "uuids", <uuids joined with space>, e.g.
    *
    *   { "uuids": "66d23424-4ec1-4775-bc15-ac937339e74f ea5347cc-1204-4222-906e-e1c292dde008 etc..." }
    *
-   * The second element is a JSON object containing one key value pair of
-   * "uuids", <uuids encoded as JSON array>, e.g.
+   * The `arr` element is a JSON object containing one key/value pair of
+   * "uuids", <JSON array of uuids>, e.g.
+   *
+   *   { "uuids": ["66d23424-4ec1-4775-bc15-ac937339e74f", "ea5347cc-1204-4222-906e-e1c292dde008", etc...] }
+   *
+   * The `encoded` element is a JSON object containing one key value pair of
+   * "uuids", <string representation of JSON array of uuids>, e.g.
    *
    *   { "uuids": "[\"66d23424-4ec1-4775-bc15-ac937339e74f\",\"ea5347cc-1204-4222-906e-e1c292dde008\",etc...]" }
    *
@@ -37,28 +45,30 @@ object JsonWriting {
       mkString: String => J,
       mkObj: Seq[(String, J)] => J,
       mkArr: Seq[J] => J,
-      writeJson: J => String): (J, J) = {
-    val plainText = mkObj(Seq("uuids" -> mkString(uuids.mkString(" "))))
-    val jsonText = mkObj(Seq("uuids" -> mkString(writeJson(mkArr(uuids.map(mkString(_)).toSeq)))))
-    (plainText, jsonText)
-  }
+      writeJson: J => String): GeneratedJson[J] =
+    GeneratedJson[J](
+      mkObj(Seq("uuids" -> mkString(uuids.mkString(" ")))),
+      mkObj(Seq("uuids" -> mkArr(uuids.map(mkString(_)).toSeq))),
+      mkObj(Seq("uuids" -> mkString(writeJson(mkArr(uuids.map(mkString(_)).toSeq))))))
 
   def main(args: Array[String]): Unit = {
     val uuids = timed("Generating UUIDs", () => List.tabulate(25000)(_ => UUID.randomUUID.toString))
 
-    val (argPlainText, argJsonText) = timed("Generating argonaut JSON", () =>
+    val argJson = timed("Generating argonaut JSON", () =>
       generateJson[AJson](uuids, AJson.jString(_), l => AJson.obj(l:_*), l => AJson.array(l:_*), _.nospaces))
 
-    val (circePlainText, circeJsonText) = timed("Generating circe JSON", () =>
+    val circeJson = timed("Generating circe JSON", () =>
       generateJson[CJson](uuids, CJson.fromString(_), l => CJson.obj(l:_*), l => CJson.arr(l:_*), _.noSpaces))
 
     println(s"\n${"*" * 70}\nArgonaut\n${"*" * 70}")
-    testJsonWriting[AJson]("plain text argonaut", argPlainText, _.nospaces, _.spaces2)
-    testJsonWriting[AJson]("JSON text argonaut", argJsonText, _.nospaces, _.spaces2)
+    testJsonWriting[AJson]("plain text argonaut", argJson.str, _.nospaces, _.spaces2)
+    testJsonWriting[AJson]("JSON array argonaut", argJson.arr, _.nospaces, _.spaces2)
+    testJsonWriting[AJson]("JSON text argonaut", argJson.encoded, _.nospaces, _.spaces2)
 
     println(s"\n${"*" * 70}\nCirce\n${"*" * 70}")
-    testJsonWriting[CJson]("plain text circe", circePlainText, _.noSpaces, _.spaces2)
-    testJsonWriting[CJson]("JSON text circe", circeJsonText, _.noSpaces, _.spaces2)
+    testJsonWriting[CJson]("plain text circe", circeJson.str, _.noSpaces, _.spaces2)
+    testJsonWriting[CJson]("JSON array circe", circeJson.arr, _.noSpaces, _.spaces2)
+    testJsonWriting[CJson]("JSON text circe", circeJson.encoded, _.noSpaces, _.spaces2)
 
     ()
   }
